3b87ce749891a62256c7a318c826baac720bde46
Albert Albala <albert@mediatribe.net>
2017-10-31 12:22:57 -0400

    Interact with the environment via a trait (#2)

--- code/src/App.php
+++ code/src/App.php
@@ -2,6 +2,7 @@
 
 namespace myproject;
 
+use myproject\traits\Environment;
 use myproject\traits\Singleton;
 
 /**
@@ -9,13 +10,34 @@ use myproject\traits\Singleton;
  */
 class App {
 
+  use Environment;
   use Singleton;
+
+  /**
+   * Prints the square root of the number of files in a directory.
+   *
+   * This method is admittedly useless, but it is meant to demonstrate how
+   * a method which uses externalities via the Environment trait can be
+   * tested.
+   *
+   * @throws \Exception
+   */
+  public function printSquareRootOfNumberOfFilesInDirectory() {
+    $directory = $this->getEnv('DIRECTORY');
+    if (!$directory) {
+      throw new \Exception('Please set the DIRECTORY environment variable.');
+    }
+    $files = $this->scanDir($directory);
+    $sqrt = $this->squareRoot(count($files));
+    $this->print($sqrt);
+  }
 
   /**
    * Returns the square root of a number.
    *
    * This is added to show an example of how the associated test code
-   * (see ./code/test/AppTest.php) works.
+   * (see ./code/test/AppTest.php) works for a pure function with no
+   * externalities.
    *
    * @param float $a
    *   The number whose square root we want to get.
--- code/test/AppTest.php
+++ code/test/AppTest.php
@@ -16,6 +16,72 @@ use myproject\App;
 class AppTest extends TestCase {
 
   /**
+   * Test for printSquareRootOfNumberOfFilesInDirectory().
+   *
+   * @param string $message
+   *   The test message.
+   * @param array $dir
+   *   Contents of the directory.
+   * @param string $expected
+   *   The expected result.
+   *
+   * @cover ::printSquareRootOfNumberOfFilesInDirectory
+   * @dataProvider providerPrintSquareRootOfNumberOfFilesInDirectory
+   */
+  public function testPrintSquareRootOfNumberOfFilesInDirectory(string $message, array $dir, string $expected) {
+    $object = $this->getMockBuilder(App::class)
+      // NULL = no methods are mocked; otherwise list the methods here.
+      ->setMethods([
+        'getEnv',
+        'scanDir',
+        'print',
+      ])
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $object->method('getEnv')
+      ->willReturn('/cannot/be/empty');
+    $object->method('scanDir')
+      ->willReturn($dir);
+    $object->expects($this->once())
+      ->method('print')
+      ->with($expected);
+
+    $object->printSquareRootOfNumberOfFilesInDirectory();
+  }
+
+  /**
+   * Provider for testPrintSquareRootOfNumberOfFilesInDirectory().
+   */
+  public function providerPrintSquareRootOfNumberOfFilesInDirectory() {
+    return [
+      [
+        'Square root of 9 is 3.0',
+        [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+        ],
+        '3.0',
+      ],
+      [
+        'Square root of 2 is 1.4142135623730951',
+        [
+          1,
+          2,
+        ],
+        '1.4142135623730951',
+      ],
+    ];
+  }
+
+  /**
    * Test for squareRoot().
    *
    * @param string $message
@@ -33,7 +99,7 @@ class AppTest extends TestCase {
    */
   public function testSquareRoot(string $message, float $input, string $exception, float $expected = 0) {
     $object = $this->getMockBuilder(App::class)
-      // NULL = no methods are mocked.
+      // NULL = no methods are mocked; otherwise list the methods here.
       ->setMethods(NULL)
       ->disableOriginalConstructor()
       ->getMock();
